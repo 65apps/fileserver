ec45ffd4eed85cb1725b85346253258ec4c88557
Andrey Ivanov <lekkion@gmail.com>
2016-03-31 11:58:36 +0400

    init commit

---
+++ client/client.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const http = require('http');
+const fs = require('fs');
+const config = require('../config/config.json');
+
+let getInfo = () => {
+    let options = {
+        port: config.port,
+        hostname: '127.0.0.1',
+        method: 'GET',
+        path: '/info'
+    };
+
+    const req = http.request(options, function(res) {
+        res.on('data', function (chunk) {
+            console.log('Response to /info', chunk.toString());
+        });
+    });
+
+    req.end();
+};
+
+let getInfoDetail = () => {
+    let options = {
+        port: config.port,
+        hostname: '127.0.0.1',
+        method: 'GET',
+        path: '/info/caucasus'
+    };
+
+    const req = http.request(options, function(res) {
+        res.on('data', function (chunk) {
+            console.log('Response to /info/caucasus', chunk.toString());
+        });
+    });
+
+    req.end();
+};
+
+let downloadFiles = () => {
+    let options = {
+        port: config.port,
+        hostname: '127.0.0.1',
+        method: 'GET',
+        path: '/download/caucasus.zip',
+        headers: {
+            'first-byte-pos': 11
+            , 'last-byte-pos': 10
+        }
+    };
+
+    const writer = fs.createWriteStream('caucasus.zip');
+    writer.on('pipe', (src) => {
+        console.log('something is piping into the writer');
+        console.log(src)
+    });
+
+    const req = http.request(options);
+    req.end();
+    req.on('response', function(mes) {
+        mes.on('data', (chunk) => {
+            writer.write(chunk);
+            console.log(chunk)
+        });
+
+        mes.read();
+    });
+};
+
+
+getInfo();
+getInfoDetail();
+downloadFiles();
---
+++ routes/index.js
@@ -0,0 +1,102 @@
+'use strict';
+
+const config = require('../config/config.json');
+const express = require('express');
+const fs = require('fs');
+
+const router = express.Router();
+
+let dir = null;
+
+readFiles()
+.then(readStat)
+.then( info => {
+  dir = info;
+})
+.catch((err) => {
+  console.log('dir read error', err);
+});
+
+router.get('/', (req, res) => {
+  res.render('index', { files: dir });
+});
+
+router.get('/info', (req, res) => {
+  res.json(dir);
+});
+
+router.get('/info/:name', (req, res) => {
+  let search = null;
+  dir.forEach( el => {
+    if (el.name == req.params.name) return search = el;
+  });
+
+  search ? res.json(search) : res.status(404).json({ result: 'Not found' });
+});
+
+router.get('/download/:file', (req, res) => {
+  let search = null;
+  dir.forEach( el => {
+    if (el.file == req.params.file) return search = el;
+  });
+
+  if (!search) return res.status(404).json({ result: 'Not found' });
+
+  let start = req.headers['first-byte-pos'] ? +req.headers['first-byte-pos'] : 0;
+  let end = req.headers['last-byte-pos'] ? +req.headers['last-byte-pos'] : search.size-1;
+
+  if (start < 0) {
+    start = 0;
+  }
+
+  if (end > search.size - 1 ) {
+    end = search.size - 1;
+  }
+
+  if (start >= end ) {
+    start = 0;
+    end = search.size - 1;
+  }
+
+  let headers = {
+    'Content-Type':'application/octet-stream'
+    , 'Content-Disposition': 'attachment; filename=' + search.file
+    , 'Accept-Ranges': 'bytes'
+    , 'Content-Length': (end - start + 1)
+  };
+
+  res.set(headers);
+  let reader = fs.createReadStream(config.files + search.file, { start: start, end: end });
+
+  reader.on('error', function(err) {
+    console.log(err);
+    res.status(500).end('Internal error');
+  });
+
+  reader.pipe(res);
+});
+
+module.exports = router;
+
+function readFiles() {
+  return new Promise( (resolve, reject) => {
+    fs.readdir(config.files, (err, files) => {
+      if(err) return reject(err);
+      resolve(files);
+    });
+  })
+}
+
+function readStat(files) {
+  return new Promise( (resolve, reject) => {
+    let info = [];
+
+    files.forEach(el => {
+      let name = el.split('.')[0];
+      let stat = fs.statSync(config.files + el);
+      info.push( { size: stat.size, name: name, file: el, ctime: stat.ctime } );
+    });
+
+    resolve(info)
+  })
+}
